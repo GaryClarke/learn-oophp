32c6686a098b4afe2c252accf9e20d2b467c3315
Gary Clarke <gary.clarke@immediate.co.uk>
2023-11-07 15:32:38 +0000

    Add name and price properties to Product

--- chapter-1/creating-classes.php
+++ chapter-1/creating-classes.php
@@ -2,6 +2,16 @@
 
 class Product
 {
-
+    public $name = 'soap';
+    public $price = 10;
 }
 
+# Challenge
+# 1. Create a price property and give it a default integer value
+# 2. Access the price on a product object and print it
+$product = new Product();
+print $product->price . "\n";
+# 3. Change the price
+$product->price = 5;
+# 4. Access the (new) price on a product object and print it
+print $product->price . "\n";
