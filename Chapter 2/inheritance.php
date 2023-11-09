33e58ad60a3b2d4affe7baab7a013336074f21c4
Iloba <ilobatimothy@gmail.com>
2023-11-09 21:50:14 +0100

    create files for inheritance challenge lesson

--- Chapter 2/inheritance.php
+++ Chapter 2/inheritance.php
@@ -4,6 +4,7 @@ require_once 'PhysicalBook.php';
 require_once 'DigitalBook.php';
 
 $physicalBook = new PhysicalBook('A Random Book', 'Jane Doe', 2000, 300);
+$digitalBook = new DigitalBook('A Random Book', 'Jane Doe', 2000, 1048);
 
-print $physicalBook->getTitle() . PHP_EOL;
-print $physicalBook->print() . PHP_EOL;
+print $digitalBook->getFileSize() . PHP_EOL;
+print $digitalBook->print() . PHP_EOL;
