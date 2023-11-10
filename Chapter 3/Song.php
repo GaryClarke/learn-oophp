8d43802922a1bd7bc445d0b91e5b861d5f2d922a
Iloba <ilobatimothy@gmail.com>
2023-11-10 23:17:01 +0100

    create files for union types lesson

--- Chapter 3/Song.php
+++ Chapter 3/Song.php
@@ -5,9 +5,27 @@ class Song
     public string $name;
     public int $numberOfPlays;
 
+    /**
+     * Song rating from 0 to 5
+     * Half ratings allowed e.g. 3.5
+     */
+    private int|float $rating = 3;
+
     public function __construct(string $name, int $numberOfPlays)
     {
         $this->name = $name;
         $this->numberOfPlays = $numberOfPlays;
+    }
+
+    public function setRating(int|float $rating): void
+    {
+        $rating = max(0, $rating);
+
+        $this->rating = min(5, $rating);
+    }
+
+    public function getRating(): int|float
+    {
+        return $this->rating;
     }
 }
