7823bc237b1456a503e3445627413b0a05133d0d
Iloba <ilobatimothy@gmail.com>
2023-11-11 00:30:45 +0100

    create files for finally block class

--- Chapter 4/exceptions.php
+++ Chapter 4/exceptions.php
@@ -4,18 +4,29 @@ require_once 'JsonFileReader.php';
 
 try {
 
+    $filePointer = fopen('errors.txt', 'a+');
+    fwrite($filePointer, "Starting...");
+
     $reader = new JsonFileReader();
-    $items = $reader->readFileAsAssociativeArray('foo.json');
+    $items = $reader->readFileAsAssociativeArray('inventory.json');
 
     print_r($items);
 
-} catch (FileNotFoundException|JsonException) {
+} catch (FileNotFoundException|JsonException $fileException) {
 
-//    print 'File problem: ' . $fileException->getMessage() . ' on line ' . $fileException->getLine() . ' in file ' . $fileException->getFile() . PHP_EOL;
+    fwrite($filePointer, "File problem: {$fileException->getMessage()} on line {$fileException->getLine()}  in file {$fileException->getFile()} \n");
 
-    print 'File problem..figure it out yourself..';
+    print 'There was a problem with your file...please check the errors.log' . PHP_EOL;
 
 } catch (Exception $exception) {
 
-    print 'A problem occurred: ' . $exception->getMessage() . ' on line ' . $exception->getLine() . ' in file ' . $exception->getFile() . PHP_EOL;
+    fwrite($filePointer, "A problem occurred: {$exception->getMessage()} on line {$exception->getLine()} in file {$exception->getFile()} \n");
+
+    print 'There was a problem...please check the errors.log' . PHP_EOL;
+
+} finally {
+
+    print "Finally block being called" . PHP_EOL;
+
+    fclose($filePointer);
 }
