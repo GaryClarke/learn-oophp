aa498e35f727e998ffb2e8bb7f2aff37e5bd113b
Iloba <ilobatimothy@gmail.com>
2023-11-14 07:35:58 +0100

    create files for pdo queries part 1 lesson

--- Chapter 8/pdo-practice.php
+++ Chapter 8/pdo-practice.php
@@ -5,3 +5,21 @@ use App\Connection;
 require_once 'vendor/autoload.php';
 
 $connection = Connection::getInstance();
+
+$pdo = $connection->getPdo();
+
+//$stmt = $pdo->query('SELECT * FROM playlist');
+
+//while($row = $stmt->fetch()) {
+//
+//    print $row['name'] . PHP_EOL;
+//}
+
+$stmt = $pdo->prepare('SELECT * FROM playlist WHERE name LIKE ?');
+$stmt->execute(['%Ballads%']);
+$playlist = $stmt->fetch();
+
+
+$stmt = $pdo->prepare('SELECT * FROM playlist WHERE name LIKE :foo AND category = :category');
+$stmt->execute(['foo' => '%Power Ballads%', 'category' => strtolower('Soft Rock')]);
+$playlist = $stmt->fetch();
