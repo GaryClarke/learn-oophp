1b5eddd954f5e27707326078bfe5ef359c6ae86a
Iloba <ilobatimothy@gmail.com>
2023-11-14 19:26:09 +0100

    create files for modifying dates lesson

--- Chapter 9/date-interval.php
+++ Chapter 9/date-interval.php
@@ -5,9 +5,15 @@ require_once 'vendor/autoload.php';
 // Compare with > and <
 $today = date_create('now');
 $saturday = date_create('saturday');
-$result = $today > $saturday;
 
 // Diff
 $inThePast = date_create('2015-04-04');
 $inTheFuture = date_create('2025-08-08');
 $interval = $inTheFuture->diff($inThePast);
+
+// modify
+$inThePast->modify('+ 5 years');
+
+// Create a DateTimeImmutable
+$dateTimeImmutable = new DateTimeImmutable('2015-04-04');
+$newDate = $dateTimeImmutable->modify('+ 5 years');
