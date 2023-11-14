1b11ae19268399bb74c491c0c87fede29fd33132
Iloba <ilobatimothy@gmail.com>
2023-11-14 07:59:18 +0100

    create files for entity creation practice lesson

--- Chapter 8/cli-config.php
+++
@@ -1,9 +0,0 @@
-<?php // cli-config.php
-
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-require_once 'vendor/autoload.php';
-
-$conn = \App\MySQL\Connection::getInstance();
-
-return ConsoleRunner::createHelperSet($conn->getEntityManager());
---
+++ Chapter 8/src/Entity/Playlist.php
@@ -0,0 +1,37 @@
+<?php // src/Entity/Playlist.php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'playlists')]
+class Playlist
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $name;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+}
