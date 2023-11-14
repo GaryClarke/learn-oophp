6632af1a7a0195b97d2209144e3fe325b03ac168
Iloba <ilobatimothy@gmail.com>
2023-11-14 19:32:26 +0100

    create files for iterator interface part two lesson

---
+++ Chapter 10/mapped-iterator.php
@@ -0,0 +1,52 @@
+<?php // src/mapped-iterator.php
+
+class MappedIteratorDemo implements Iterator
+{
+
+    public function __construct(private array $items = [])
+    {
+    }
+
+    public function current(): mixed
+    {
+        return current($this->items);
+    }
+
+    public function next(): void
+    {
+        next($this->items);
+    }
+
+    public function key(): mixed
+    {
+        return key($this->items);
+    }
+
+    public function valid(): bool
+    {
+        return key($this->items) !== null;
+    }
+
+    public function rewind(): void
+    {
+        reset($this->items);
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+}
+
+$mappedArray = [
+    'name' => 'Gary',
+    'location' => 'London, UK',
+    'role' => 'Software Developer'
+];
+
+$mappedIteratorDemo = new MappedIteratorDemo($mappedArray);
+
+foreach ($mappedIteratorDemo as $item => $value) {
+
+    print "$item: $value" . PHP_EOL;
+}
