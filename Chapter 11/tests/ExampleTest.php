5c7a415231715837e62413c3c7071715080a10c2
Iloba <ilobatimothy@gmail.com>
2023-11-14 19:54:08 +0100

    create files for php unit set up lesson

---
+++ Chapter 11/tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php // tests/ExampleTest.php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    public function testItWorks(): void
+    {
+        $this->assertTrue(1 === 2);
+    }
+}
