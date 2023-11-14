3fff15d2c112bba909f0b4cc1d095c0331a15240
Iloba <ilobatimothy@gmail.com>
2023-11-14 19:22:00 +0100

    create files for unserialize magic method lesson

--- Chapter 9/__serialize.php
+++ Chapter 9/__serialize.php
@@ -5,3 +5,11 @@ require_once 'vendor/autoload.php';
 $user = new \App\User(username: 'garyclarketech', password: 'password');
 
 $serializedUser = serialize($user);
+
+$unserializedUser = unserialize($serializedUser);
+
+dd($unserializedUser);
+
+// Add __sleep and __wakeup methods to User
+// Add echo statements to print out when they are being called
+// What do you observe?
--- Chapter 9/src/User.php
+++ Chapter 9/src/User.php
@@ -1,4 +1,4 @@
-<?php // src/User.php
+<?php
 
 namespace App;
 
@@ -10,9 +10,33 @@ class User
 
     public function __serialize(): array
     {
+        echo 'Serializing user with __serialize' . PHP_EOL;
+
         return [
             'user' => $this->username,
             'pass' => $this->password
         ];
+    }
+
+    public function __sleep(): array
+    {
+        echo 'Going to sleep...' . PHP_EOL;
+
+        return ['username', 'password'];
+    }
+
+    public function __wakeup(): void
+    {
+        echo 'Waking up,,,' . PHP_EOL;
+    }
+
+    public function __unserialize(array $data): void
+    {
+        echo 'Unserializing user with __unserialize' . PHP_EOL;
+
+        $this->username = $data['user'];
+        $this->password = $data['pass'];
+
+        echo 'Doing anything else which is necessary...' . PHP_EOL;
     }
 }
