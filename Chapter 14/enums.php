6e94ba051294760b96026835a4c00b4b5bf220c0
Iloba <ilobatimothy@gmail.com>
2023-11-16 19:47:47 +0100

    create files for pure enums vs backed enums lesson

--- Chapter 14/enums.php
+++ Chapter 14/enums.php
@@ -1,17 +1,32 @@
 <?php // enums.php
 
+enum HttpStatusCode: int
+{
+    case Ok = 200;
+    case Created = 201;
+    case BadRequest = 400;
+    case Unauthorized = 401;
+    case Forbidden = 403;
+    case InternalServerError = 500;
+}
+
 class Response
 {
-    public const HTTP_OK = 200;
-  
+    const HTTP_OK = 200;
+
     public function __construct(
         private ?string $content,
-        private int $status, // can't be random..fixed number of status codes all with a meaning
+        private HttpStatusCode $statusCode, // can't be random..fixed number of status codes all with a meaning
         private array $headers
     ) {
     }
+
+    public function getStatusCodeValue(): int
+    {
+        return $this->statusCode->value;
+    }
 }
 
-$response = new Response('Some content', Response::HTTP_OK, []);
+$response = new Response('Some content', HttpStatusCode::Created, []);
 
-$response = new Response('Some content', 6789, []);
+echo $response->getStatusCodeValue() . PHP_EOL;
